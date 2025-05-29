925a3214926692b941396313e78349cf22687533
Edin <edin.vatrich@gmail.com>
2025-05-29 16:25:15 +0200

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+//mongo URI
+//const MONGO_URI="mongodb+srv://user:user@cluster0.w0exw.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI="mongodb://atlas-sql-658700a1e00058779f655976-w0exw.a.query.mongodb.net/test?ssl=true&authSource=admin;"
+
 
 const app = express();
+
+app.use(express.json());
+
+
 
 //basic route
 app.get("/", (req,res)=>{
@@ -11,7 +21,12 @@ app.get("/", (req,res)=>{
 const port = 4000;
 
 
-//listening on port 4000
+//connect to db
+mongoose.connect(MONGO_URI)
+.then(()=>{
 app.listen(port, ()=>{
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`)
+})})
+.catch((error)=>{
+    console.log(error)
 })
