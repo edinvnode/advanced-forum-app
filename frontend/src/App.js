f57d23f873c54e64b67a85225de7d6336d815cf9
Edin <edin.vatrich@gmail.com>
2025-05-31 22:55:51 +0200

    Added profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { titles } from './data';
 //components
 import LoginForm from './components/LoginForm';
 import MainPage from './components/MainPage';
+import Profile from './components/Profile';
 
 //router
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
@@ -16,10 +17,14 @@ function App() {
           <Link to="/login" className="login-link">
             Login
           </Link>
+          <Link to="/" className="login-link">
+            Main Page
+          </Link>
         </nav>
         <Routes>
-          <Route path="/login" element={<LoginForm />} />
           <Route path="/" exact element={<MainPage titles={titles} />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </div>
