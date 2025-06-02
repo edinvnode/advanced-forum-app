2ba0fc99926ccb40f6adce81d6002fd570a1b669
Edin <edin.vatrich@gmail.com>
2025-06-02 23:08:53 +0200

    CSS flex experiment

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 const LoginForm = () => {
   return (
-    <form>
-      <div>
-        <label>Username</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" />
-      </div>
-      <div>
-        <button>Submit</button>
-      </div>
-    </form>
+    <div className="form">
+      <form>
+        <div>
+          <label>Username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
