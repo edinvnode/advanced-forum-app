8474d29c7e2268b56f59cd4ff9d7c938f0b03a5b
Edin <edin.vatrich@gmail.com>
2025-06-09 18:39:45 +0200

    First state

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(email);
+    alert(password);
+  };
+
   return (
     <div className="form">
       <form>
         <p>Enter your username and password</p>
         <div>
           <label className="lbl-email">Email</label>
-          <input type="email" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label className="lbl-password">Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
-          <button>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
       </form>
     </div>
