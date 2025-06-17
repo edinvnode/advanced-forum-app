377b76de9c41121849961b51523c318b7eb4b910
Edin <edin.vatrich@gmail.com>
2025-06-17 23:12:56 +0200

    Form update

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -4,10 +4,44 @@ const Create = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  /*
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email);
     console.log(password);
+  };
+
+  */
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!user) {
+      setError('You must be logged in');
+      return;
+    }
+
+    const workout = { title, load, reps };
+
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
+    }
+    if (response.ok) {
+      setEmail('');
+      setPassword('');
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
+    }
   };
 
   return (
