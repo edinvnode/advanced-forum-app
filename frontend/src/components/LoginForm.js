23080566e3dac10eb159b2785a31e7df413cf230
Edin <edin.vatrich@gmail.com>
2025-06-27 23:06:21 +0200

    Login form

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,13 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../../App';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { state, dispatch } = useContext(UserContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
-    console.log(password);
+  const handleSubmit = () => {
+    /*
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      M.toast({ html: 'invalid Email ID', classes: 'red darken-3' });
+      return;
+    } */
+
+    fetch('/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          //M.toast({ html: data.error, classes: 'red darken-3' });
+        } else {
+          localStorage.setItem('jwt', data.token);
+          localStorage.setItem('user', JSON.stringify(data.user));
+          dispatch({ type: 'USER', payload: data.user });
+          //M.toast({ html: data.message, classes: 'deep-purple lighten-3' });
+          // history.push('/');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
